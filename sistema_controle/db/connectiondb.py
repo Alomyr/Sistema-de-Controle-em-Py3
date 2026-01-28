3fb2bdd2f0dd30bc211491b14ba9edee94f4ced8
Alomyr <matheuscastro11213@gmail.com>
2026-01-27 23:07:02 -0300

    sobe alteracoes no nome do database

--- sistema_controle/db/connectiondb.py
+++ sistema_controle/db/connectiondb.py
@@ -4,7 +4,7 @@ import psycopg2
 def connection():
     return psycopg2.connect(
         host="127.0.0.1",
-        database="sistama de tarefas",
+        database="sistema_de_tarefas",
         user="postgres",
         password="123456",
         port=5432,
